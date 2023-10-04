ceb4640dd90343fa82d2885947b02d87042a8a9d
Dessian-Guiro <135682316+Dessian-Guiro@users.noreply.github.com>
2023-10-03 19:12:26 -0500

    add1

--- javascript.js
+++ javascript.js
@@ -1,10 +1,24 @@
 
 
 const userInputString = prompt("Enter froyo flavors separated by commas:");
-constflavors= ("vanilla,vanilla, vanilla, strawberry, coffee,coffee:");
+const flavors = ["vanilla", "vanilla", "vanilla", "strawberry", "coffee", "coffee"];
+const flavorFrequencies = countFrequencies(flavors);
+console.log(flavorFrequencies);
 
+function countFrequencies(arr) {
+    const frequencyMap = {}; 
+    arr.forEach((item) => {
+        if (frequencyMap[item]) {
+           
+            frequencyMap[item]++;
+        } else {
+           
+            frequencyMap[item] = 1;
+        }
+    });
 
-
+    return frequencyMap; // I Returned the object with frequencies
+}
 
 
 
@@ -24,4 +38,20 @@ constflavors = prompt("Enter froyo flavors separated by commas:");
 const flavorsArray = userInput.split(",");
 trackFlavors(flavorsArray);
 
-console.log(observedFlavors);
+
+
+ 
+function parseFlavors() {
+    var inputText = document.getElementById('flavorsInput').value;
+    var flavorsArray = inputText.split(',');
+
+    var observedFlavors = {};
+
+    flavorsArray.forEach(function(flavor) {
+      if (!observedFlavors.hasOwnProperty(flavor.trim())) {
+        observedFlavors[flavor.trim()] = true;
+      }
+    });
+
+    console.log(observedFlavors);
+  }
